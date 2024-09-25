79114229084229cd31cdf9123a5efa94ea1873de
Emily Wu <emilywu@Emilys-MBP.lan>
2024-09-24 21:33:33 -0400

    render result from backend

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Board = () => {
-  const mockBoard = Array.from(Array(8), () => new Array(8).fill(null));
-  const [board, setBoard] = useState(mockBoard);
+  const [board, setBoard] = useState<string[][]>();
 
+  useEffect(() => {
+    const getBoard = async () => {
+      await fetch(
+        "http://localhost:8080/game/55be3af7-7ec4-4947-9f59-182aff592914"
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          setBoard(res.board);
+        });
+    };
+    getBoard();
+  }, []);
+
+  console.log("board", board);
+  if (!board) return <></>;
   return (
     <div>
-      {board.map((row) => (
-        <div className="board">
-          {row.map((tile) => (
-            <div className="tile">{tile}</div>
+      {board.map((row, i) => (
+        <div className="board" key={i}>
+          {row.map((tile, j) => (
+            <div className="tile" key={j}>
+              {tile}
+            </div>
           ))}
         </div>
       ))}
